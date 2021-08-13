a3691f89152254285418e36d5b74803021f87f12
Piet van Blokland <pjvanblokland@gmail.com>
2021-08-13 20:12:35 +0200

    first stage

---
+++ src/components/UpperCenter.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+
+</div>
+</template>
+<script>
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UpperCenter",
+  data(){
+    return {}
+  }//,
+  //props:{dataList:Array} 
+  
+})
+
+</script>
+
